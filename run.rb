319331ad1504e1646aedfca86a1af1de07ccf41b
Amanda <ajnikrant@gmail.com>
2021-03-01 18:23:41 -0500

    commit tree 30750cdced400fa54f316f0d40d014addf13111d

--- run.rb
+++ run.rb
@@ -3,8 +3,17 @@ require_relative "lib/Zoo.rb"
 require 'pry'
 
 
-#Test your code here
+zoo1=Zoo.new("San Diego Zoo", "California")
+zoo2=Zoo.new("Australia Zoo", "Sydney")
+zoo3=Zoo.new("LA Zoo", "California")
+zoo4=Zoo.new("Sacremento Zoo", "California")
 
+animal1=Animal.new("equine", 200, "horse", zoo1)
+animal2=Animal.new("bovine", 50, "baby cow", zoo2)
+animal3=Animal.new("equine", 35, "baby horse", zoo1)
+animal4=Animal.new("equine", 250, "other horse", zoo2)
+animal5=Animal.new("canine", 30, "dog", zoo1)
+animal6=Animal.new("feline", 6, "cat", zoo1)
 
 binding.pry
 puts "done"
